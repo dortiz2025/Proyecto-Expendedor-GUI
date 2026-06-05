63bcc62972d37980e77be10870b9d31cc0d0faa5
dortiz2025 <dortiz2025@inf.udec.cl>
2026-06-05 13:44:54 -0400

    Se añade método size a Deposito. Útil para poder saber cuantos elementos dibujar en la interfaz.

--- src/main/java/expendedor/logica/Deposito.java
+++ src/main/java/expendedor/logica/Deposito.java
@@ -3,39 +3,44 @@ package expendedor.logica;
 import java.util.ArrayList;
 
 /**
- * Depósito Genérico
- *
- * @param <T> Clase que será elemento del depósito
+ * Depósito Genérico.
+ * @param <T> Clase que será elemento del depósito.
  */
 public class Deposito <T> {
 
     /**
      * Arreglo que almacena elementos de clase T
-     * simulando un depósito
+     * simulando un depósito.
      */
     private ArrayList<T> deposito;
 
     /**
-     * Constructor que inicializa el ArrayList
+     * Constructor que inicializa el ArrayList.
      */
     public Deposito() {
         this.deposito = new ArrayList<>();
     }
 
     /**
-     * Metodo que añade elementos al depósito
-     *
-     * @param elemento Elemento que se quiere añadir al depósito
+     * Permite a la GUI saber cuántos elementos hay para dibujarlos.
+     * @return Cantidad de elementos en el depósito.
+     */
+    public int size(){
+        return this.deposito.size();
+    }
+
+    /**
+     * Metodo que añade elementos al depósito.
+     * @param elemento Elemento que se quiere añadir al depósito.
      */
     public void add(T elemento) {
         this.deposito.add(elemento);
     }
 
     /**
-     * Retira elementos del depósito
-     * Mecánica FIFO
-     *
-     * @return Devuelve el elemento retirado del depósito
+     * Retira elementos del depósito.
+     * Mecánica FIFO.
+     * @return Entrega el elemento retirado del depósito.
      */
     public T get() {
         if (deposito.isEmpty()) {
