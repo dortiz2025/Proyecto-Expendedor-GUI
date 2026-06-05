9fbaffe5e61ea7c5a5604edfa6e66baa723aed3f
dortiz2025 <dortiz2025@inf.udec.cl>
2026-06-05 15:22:12 -0400

    Constructor de Comprador ahora solo inicializa el monedero con unas cuantas monedas.

--- src/main/java/expendedor/logica/Comprador.java
+++ src/main/java/expendedor/logica/Comprador.java
@@ -11,9 +11,6 @@ import expendedor.logica.productos.*;
  * y cuanto vuelto le dieron
  */
 public class Comprador {
-
-    private String productoConsumido;
-
     //Monedero
     private int cantMonedas1500;
     private int cantMonedas1000;
@@ -24,47 +21,13 @@ public class Comprador {
     private Producto inventario;
 
     /**
-     * Constructor de la clase que simula la compra de un producto
-     *
-     * @param moneda Medio de pago para la compra (Puede ser null)
-     * @param producto Producto que se ha elegido comprar
-     * @param expendedor Máquina con la que se interactúa
-     *
-     * @throws NoHayProductoException Si no hay stock del producto o no existe
-     * @throws PagoInsuficienteException Si el dinero no alcanza
-     * @throws PagoIncorrectoException Si se compra sin dinero (Moneda null)
+     * Constructor de la clase comprador
      */
-    public Comprador(Moneda moneda, TipoProducto producto, Expendedor expendedor) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
-
-        // Variable que almacena el producto comprado.
-        // Puede ser null si hubo una excepción.
-        Producto productoComprado = null;
-
-        try {
-            // Se hace la compra del producto seleccionado.
-            // Se guarda en la variable.
-            productoComprado = expendedor.comprarProducto(moneda, producto);
-        } finally {
-
-            // Variable que almacena el vuelto.
-            // Moneda a moneda.
-            Moneda vueltoMoneda;
-
-            // Se pide vuelto
-            // Mientras no sea null -> Se sigue pidiendo
-            vueltoMoneda = expendedor.getVuelto();
-            this.vueltoCompra = 0; //Se comienza en 0 de vuelto
-            while (vueltoMoneda != null) {
-                //Se suma al vuelto total de la compra
-                this.vueltoCompra += vueltoMoneda.getValor();
-                vueltoMoneda = expendedor.getVuelto();
-            }
-
-            // Si existe un producto comprado, se guarda su nombre
-            // Sino, queda null
-            if (productoComprado != null) this.productoConsumido = productoComprado.consumir();
-            else this.productoConsumido = null;
-        }
+    public Comprador(Moneda moneda, TipoProducto producto, Expendedor expendedor) {
+        this.cantMonedas1500 = 5;
+        this.cantMonedas1000 = 5;
+        this.cantMonedas500 = 4;
+        this.cantMonedas100 = 10;
     }
 
     /**
