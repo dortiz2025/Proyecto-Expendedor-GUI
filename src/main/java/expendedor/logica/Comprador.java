e4f6758fc35a24e4efcd24cd1de7807de71a9154
dortiz2025 <dortiz2025@inf.udec.cl>
2026-06-05 15:24:13 -0400

    Atributos producto consumido y vuelto se eliminan junto con sus métodos respectivos.

--- src/main/java/expendedor/logica/Comprador.java
+++ src/main/java/expendedor/logica/Comprador.java
@@ -21,7 +21,7 @@ public class Comprador {
     private Producto inventario;
 
     /**
-     * Constructor de la clase comprador
+     * Constructor de la clase comprador.
      */
     public Comprador(Moneda moneda, TipoProducto producto, Expendedor expendedor) {
         this.cantMonedas1500 = 5;
@@ -31,28 +31,11 @@ public class Comprador {
     }
 
     /**
-     * Permite añadir un producto al inventario
+     * Permite añadir un producto al inventario.
      * @param producto producto adquirido.
      */
     public void recibirProducto(Producto producto){
         inventario = producto;
     }
 
-    /**
-     * Metodo que simula ver el vuelto recibido
-     *
-     * @return Devuelve el vuelto recibido
-     */
-    public int cuantoVuelto() {
-        return vueltoCompra; }
-
-    /**
-     * Metodo que simula preguntar que producto se ha consumido
-     *
-     * @return Entrega el nombre del producto consumido
-     */
-    public String queConsumiste() {
-        if (productoConsumido != null) return productoConsumido;
-        else return "No hay producto consumido.";
-    }
 }
