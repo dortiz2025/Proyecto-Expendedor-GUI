f7e0d6c275f1589012a5368ed439fce2789cf207
dortiz2025 <dortiz2025@inf.udec.cl>
2026-06-06 19:26:04 -0400

    Se añaden atributos de subpaneles a PanelExpendedor.

--- src/main/java/expendedor/gui/PanelExpendedor.java
+++ src/main/java/expendedor/gui/PanelExpendedor.java
@@ -8,8 +8,21 @@ import java.awt.*;
 public class PanelExpendedor extends JPanel {
     private Expendedor expendedor;
 
+    //Sub-Paneles
+    private PanelVitrina panelVitrina;
+    private PanelPago panelPago;
+    private PanelDepositoGanancias panelDepositoGanancias;
+    private PanelProductoComprado panelProductoComprado;
+    private PanelVuelto panelVuelto;
+
     public PanelExpendedor(Expendedor expendedor) {
         this.expendedor = expendedor;
+
+        this.panelVitrina =  new PanelVitrina(expendedor);
+        this.panelPago =  new PanelPago(expendedor);
+        this.panelDepositoGanancias = new PanelDepositoGanancias(expendedor);
+        this.panelProductoComprado = new PanelProductoComprado(expendedor);
+        this.panelVuelto = new PanelVuelto(expendedor);
     }
 
     @Override
