32f2dc4a12960621f257ac6dd5e6aef610f0ceb7
dortiz2025 <dortiz2025@inf.udec.cl>
2026-06-05 22:47:19 -0400

    Se crea el constructor de la ventana haciendo set de características bases para el buen funcionamiento de la gui.

--- src/main/java/expendedor/gui/Ventana.java
+++ src/main/java/expendedor/gui/Ventana.java
@@ -3,5 +3,16 @@ package expendedor.gui;
 import javax.swing.*;
 
 public class Ventana extends JFrame {
-    public Ventana() {}
+    public Ventana() {
+        this.setTitle("Expendedor Interactivo");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 430); //Se suma 30 a nuestro ideal por la barra superior
+        this.setLocationRelativeTo(null); // Centra la ventana en la pantalla
+        this.setResizable(false);
+
+        // Se agrega el panel principal
+        this.add(new PanelPrincipal());
+
+        this.setVisible(true);
+    }
 }
