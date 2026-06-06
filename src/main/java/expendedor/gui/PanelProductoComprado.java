086c35f8f05a8b74dc1ca61fbd0df70fb7fb2dba
dortiz2025 <dortiz2025@inf.udec.cl>
2026-06-06 19:18:08 -0400

    Creación subpaneles de PanelExpendedor.

---
+++ src/main/java/expendedor/gui/PanelProductoComprado.java
@@ -0,0 +1,13 @@
+package expendedor.gui;
+
+import expendedor.logica.Expendedor;
+
+import javax.swing.*;
+
+public class PanelProductoComprado extends JPanel {
+    private Expendedor expendedor;
+
+    public PanelProductoComprado(Expendedor expendedor) {
+        this.expendedor = expendedor;
+    }
+}
---
+++ src/main/java/expendedor/gui/PanelVitrina.java
@@ -0,0 +1,13 @@
+package expendedor.gui;
+
+import expendedor.logica.Expendedor;
+
+import javax.swing.*;
+
+public class PanelVitrina extends JPanel {
+    private Expendedor expendedor;
+
+    public PanelVitrina(Expendedor expendedor) {
+        this.expendedor = expendedor;
+    }
+}
