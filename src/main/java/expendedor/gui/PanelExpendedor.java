5d8ea1a4712ce1d09bea97beae489d0c4b74109c
dortiz2025 <dortiz2025@inf.udec.cl>
2026-06-06 19:49:09 -0400

    Se definen fronteras de subpaneles en PanelExpendedor. Se añaden al Panel.

--- src/main/java/expendedor/gui/PanelExpendedor.java
+++ src/main/java/expendedor/gui/PanelExpendedor.java
@@ -17,12 +17,30 @@ public class PanelExpendedor extends JPanel {
 
     public PanelExpendedor(Expendedor expendedor) {
         this.expendedor = expendedor;
+        this.setLayout(null); // Diseño libre
+        this.setBackground(new Color(0, 75, 115));//Fondo Expendedor
 
+        //Se inicializan los sub-paneles
         this.panelVitrina =  new PanelVitrina(expendedor);
         this.panelPago =  new PanelPago(expendedor);
         this.panelDepositoGanancias = new PanelDepositoGanancias(expendedor);
         this.panelProductoComprado = new PanelProductoComprado(expendedor);
         this.panelVuelto = new PanelVuelto(expendedor);
+
+        //Se definen los tamaños de cada panel de modo que estén ordenados
+        //Las coordenadas son relativas a las del expendedor en sí
+        this.panelVitrina.setBounds(10, 10, 160, 180);
+        this.panelPago.setBounds(180, 10, 60, 180);
+        this.panelDepositoGanancias.setBounds(10, 200, 80, 70);
+        this.panelProductoComprado.setBounds(105, 240, 60, 40);
+        this.panelVuelto.setBounds(180, 200, 60, 70);
+
+        //Se añaden al expendedor
+        this.add(panelVitrina);
+        this.add(panelPago);
+        this.add(panelDepositoGanancias);
+        this.add(panelProductoComprado);
+        this.add(panelVuelto);
     }
 
     @Override
