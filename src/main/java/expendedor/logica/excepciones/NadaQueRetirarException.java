7e9d31872e994ea65fbf0800aee8de451ebe9695
dortiz2025 <dortiz2025@inf.udec.cl>
2026-06-08 13:55:51 -0400

    Se añade excepcion para uso futuro.

--- src/main/java/expendedor/logica/excepciones/NadaQueRetirarException.java
+++ src/main/java/expendedor/logica/excepciones/NadaQueRetirarException.java
@@ -1,16 +1,16 @@
 package expendedor.logica.excepciones;
 
 /**
- * Excepción al intentar comprar sin haber retirado un producto ya comprado anteriormente.
+ * Excepción al intentar sacar una moneda o un producto cuando no hay nada.
  */
-public class ProductoSinRetirarException extends RuntimeException {
+public class NadaQueRetirarException extends RuntimeException {
     /**
      * Constructor de la excepción
      * Crea una instancia con una advertencia del error.
      *
      * @param mensaje Entrega razón de la excepción.
      */
-    public ProductoSinRetirarException(String mensaje) {
+    public NadaQueRetirarException(String mensaje) {
         super(mensaje);
     }
 }
