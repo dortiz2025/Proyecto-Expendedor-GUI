6a73e68180636b11b1fdd4c975ee2f7424a89c68
b33nj4 <bensilva2024@udec.cl>
2026-06-08 05:53:40 -0400

    Añadir mouse listener para consumir producto en el inventario

--- src/main/java/expendedor/gui/PanelInventario.java
+++ src/main/java/expendedor/gui/PanelInventario.java
@@ -12,6 +12,17 @@ public class PanelInventario extends JPanel {
     private Comprador comprador;
 
     public PanelInventario(Comprador comprador) {
+
         this.comprador = comprador;
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (comprador.getInventario() != null){
+                    System.out.println("Hiciste click en el inventario");
+                    comprador.consumirProducto();
+                    repaint();
+                }
+            }
+        });
     }
 }
