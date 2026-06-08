f7d5ea99156e162ccfa34f5b85de456c2a1f89d9
b33nj4 <bensilva2024@udec.cl>
2026-06-08 05:50:58 -0400

    Importar lógica de productos, MouseEvent y MouseAdapter

--- src/main/java/expendedor/gui/PanelInventario.java
+++ src/main/java/expendedor/gui/PanelInventario.java
@@ -1,8 +1,12 @@
 package expendedor.gui;
 
 import expendedor.logica.Comprador;
+import expendedor.logica.productos.Producto;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelInventario extends JPanel {
     private Comprador comprador;
