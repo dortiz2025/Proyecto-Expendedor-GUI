07e9655bc58dd7328c42861ad5728de21c445903
b33nj4 <bensilva2024@udec.cl>
2026-06-08 05:44:34 -0400

    Inicializar los sub-paneles con colores de fondo, lógica y espacios

--- src/main/java/expendedor/gui/PanelComprador.java
+++ src/main/java/expendedor/gui/PanelComprador.java
@@ -14,6 +14,23 @@ public class PanelComprador extends JPanel {
 
     public PanelComprador(Comprador comprador, Expendedor expendedor) {
         this.comprador = comprador;
+
+        this.setLayout(null);
+
+        this.setBackground(new Color(210, 235, 210));
+
+        //Inicializamos los sub-paneles pasándoles la lógica del comprador
+        this.panelMonedero = new PanelMonedero(comprador, expendedor);
+        this.panelInventario = new PanelInventario(comprador);
+
+        //Se deja un espacio vertical para las monedas
+        this.panelMonedero.setBounds(10, 10, 100, 300);
+        //Cuadro para el inventario
+        this.panelInventario.setBounds(460, 50, 99, 99);
+
+        //Añadimos ambos sub-paneles a mostrar
+        this.add(panelMonedero);
+        this.add(panelInventario);
     }
 
     @Override
