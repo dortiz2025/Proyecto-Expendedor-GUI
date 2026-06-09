5edd8603556872d436d8e55ede8bac0fbd1059fa
b33nj4 <bensilva2024@udec.cl>
2026-06-09 11:28:24 -0400

    Añadir el constructor del PanelRestock con el nuevo botón de restock, junto a un listener. Al presionar el botón se llama al método Restock de Expendedor

--- src/main/java/expendedor/gui/PanelRestock.java
+++ src/main/java/expendedor/gui/PanelRestock.java
@@ -1,12 +1,41 @@
 package expendedor.gui;
 
-public class Panelrestock {
-    public Panelrestock(){
 import expendedor.logica.Expendedor;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+/**
+ * Panel que contiene el botón para rellenar los productos de la máquina.
+ */
+public class PanelRestock extends JPanel {
+    private Expendedor expendedor;
+    private JButton botonRestock;
+
+    //Recibe el Expendedor completo
+    public PanelRestock(Expendedor expendedor) {
+        this.expendedor = expendedor;
+        this.setOpaque(false);
+        this.setLayout(new BorderLayout());
+
+        //Creamos el botón
+        botonRestock = new JButton("Restock");
+
+        //Añadimos el listener
+        botonRestock.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Llamamos al método Restock para rellenar el expendedor
+                expendedor.Restock(5); // 5 es el máximo de productos
+
+                //Repintamos toda la ventana para que los productos aparezcan visualmente
+                SwingUtilities.getWindowAncestor(PanelRestock.this).repaint();
+            }
+        });
+
+        //Añadimos el botón al panel
+        this.add(botonRestock, BorderLayout.CENTER);
 
     }
 }
