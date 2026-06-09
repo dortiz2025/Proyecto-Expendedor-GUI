5e306ae42e3d1f0b8e59fdde756acc7f9ce1deb5
MartinBastias <mbastias2005@gmail.com>
2026-06-09 13:10:30 -0400

    documentacion ventana

--- src/main/java/expendedor/gui/Ventana.java
+++ src/main/java/expendedor/gui/Ventana.java
@@ -3,17 +3,21 @@ package expendedor.gui;
 import javax.swing.*;
 
 /**
- * Clase que representa a la ventana sobre la cual se monta el PanelPrincipal
+ * Clase contenedora que representa la ventana principal de la aplicación
+ * sobre la cual se monta el entorno gráfico.
  */
 public class Ventana extends JFrame {
+
+    /**
+     * Inicializa y configura las dimensiones y propiedades de la ventana de la aplicación.
+     */
     public Ventana() {
         this.setTitle("Expendedor Interactivo");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1200, 830); // 800 + 30 de la barrapor la barra superior
-        this.setLocationRelativeTo(null); // Centra la ventana en la pantalla
+        this.setSize(1200, 830);
+        this.setLocationRelativeTo(null);
         this.setResizable(false);
 
-        // Se agrega el panel principal
         this.add(new PanelPrincipal());
 
         this.setVisible(true);
