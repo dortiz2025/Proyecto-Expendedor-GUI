4b58485fab9f84e21e69260b56a03774e9acdff6
MartinBastias <mbastias2005@gmail.com>
2026-06-09 13:09:14 -0400

    documentacion panel restock

--- src/main/java/expendedor/gui/PanelRestock.java
+++ src/main/java/expendedor/gui/PanelRestock.java
@@ -7,35 +7,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Panel que contiene el botón para rellenar los productos de la máquina.
+ * Panel de mantenimiento que permite reabastecer el inventario interno de la máquina expendedora.
  */
 public class PanelRestock extends JPanel {
     private Expendedor expendedor;
     private JButton botonRestock;
 
-    //Recibe el Expendedor completo
+    /**
+     * Inicializa el panel y configura el botón de recarga de productos.
+     * @param expendedor Referencia al expendedor lógico a reabastecer.
+     */
     public PanelRestock(Expendedor expendedor) {
         this.expendedor = expendedor;
         this.setOpaque(false);
         this.setLayout(new BorderLayout());
 
-        //Creamos el botón
         botonRestock = new JButton("Restock");
 
-        //Añadimos el listener
         botonRestock.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Llamamos al método Restock para rellenar el expendedor
-                expendedor.Restock(5); // 5 es el máximo de productos
-
-                //Repintamos toda la ventana para que los productos aparezcan visualmente
+                expendedor.Restock(5);
                 SwingUtilities.getWindowAncestor(PanelRestock.this).repaint();
             }
         });
 
-        //Añadimos el botón al panel
         this.add(botonRestock, BorderLayout.CENTER);
-
     }
 }
