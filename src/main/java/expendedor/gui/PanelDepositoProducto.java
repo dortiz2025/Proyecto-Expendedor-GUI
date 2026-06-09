6209edd77678c0751eb70523ae270ef40b081bf4
MartinBastias <mbastias2005@gmail.com>
2026-06-09 12:59:51 -0400

    documentacion paneldepositoproducto

--- src/main/java/expendedor/gui/PanelDepositoProducto.java
+++ src/main/java/expendedor/gui/PanelDepositoProducto.java
@@ -7,38 +7,35 @@ import java.awt.*;
 
 /**
  * Sub-panel de PanelVitrina.
- * Clase que permite reutilizar código al dibujar
- * los depósitos de productos.
+ * Clase que dibuja los depósitos de productos de forma genérica.
  * @param <T> Tipo de depósito genérico.
  */
 public class PanelDepositoProducto<T> extends JPanel {
-    private final Deposito<T> deposito; //Deposito genérico
-    private final Image textura;//Guarda la textura para no cargarla de nuevo
+    private final Deposito<T> deposito;
+    private final Image textura;
 
     /**
-     * Se guardan datos necesarios para poder dibujar el depósito.
+     * Inicializa los datos necesarios para renderizar el depósito.
      * @param deposito Referencia del depósito.
      * @param textura Textura cargada del producto.
      */
     public PanelDepositoProducto(Deposito<T> deposito, Image textura) {
         this.deposito = deposito;
         this.textura = textura;
-
         this.setOpaque(false);
     }
 
     /**
-     * Actualiza la cantidad de productos que hay disponibles en el expendedor.
+     * Dibuja los productos disponibles en el expendedor.
      * @param g Entorno gráfico.
      */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //Se dibujan los productos
+
         if (this.textura != null && this.deposito != null) {
             int cantidad = this.deposito.size();
             for (int i = 0; i < cantidad; i++) {
-                //Se dibuja cada producto encima de otro con posiciones diferentes.
                 g.drawImage(this.textura, 5 + (i * 10), 5, this);
             }
         }
